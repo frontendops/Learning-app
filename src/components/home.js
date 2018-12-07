e521e71a9875612d41370bbbf36ebb0c4800123b
Alejandro-Sierra <alexsierra1598@gmail.com>
2018-12-07 18:31:22 -0500

    Home page component and 2 main buttons

---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+import React, {Component}  from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="">quizzly</h1>
+                <button type="button" class="btn btn-primary">Primary</button>
+                <button type="button" class="btn btn-primary">View apps</button>
+
+                
+            </div>
+        )
+    }
+}
+
+export default Home;
