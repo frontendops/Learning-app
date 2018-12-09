e59a6360c7d2b1ccb515747372fca8c7c8e95633
Alejandro-Sierra <alexsierra1598@gmail.com>
2018-12-09 17:32:31 -0500

    Number of Questions display corresponding question input fields

--- src/components/questionForm.js
+++ src/components/questionForm.js
@@ -15,6 +15,19 @@ class QuestionForm extends Component {
         return (
             <div>
                 
+                <div className="input-group">
+                <input type="text" className="form-control" aria-label="Text input with dropdown button" placeholder="What is your Question?"></input>
+                    <div className="input-group-append">
+                        <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options</button>
+                        <div className="dropdown-menu">
+                        <a className="dropdown-item" href="#">2</a>
+                        <a className="dropdown-item" href="#">3</a>
+                        <a className="dropdown-item" href="#">4</a>
+                        <a className="dropdown-item" href="#">Separated link</a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         )
     }
--- src/components/questionsPage.js
+++ src/components/questionsPage.js
@@ -1,21 +1,31 @@
 import React, {Component}  from 'react';
+import QuestionForm from './questionForm.js';
 
 class QuestionsPage extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
+            
         }
         
     }
 
 
+
     render() {
+        let forms = [];
+
+        for (let i = 0; i < this.props.numValue; i++) {
+            forms.push(<div id={i} className="form"> <QuestionForm /> </div>);
+        }
+
         return (
             <div>
                 <h1>The quiz name is: {this.props.title}</h1>
                 <h2>There are {this.props.numValue} Questions </h2>
+
+               {forms}
                 
             </div>
         )
