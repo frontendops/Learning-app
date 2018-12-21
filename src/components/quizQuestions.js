b72a4340580b699b4ea6e708c215d13f9cd6c90b
Alejandro-Sierra <alexsierra1598@gmail.com>
2018-12-21 14:29:50 -0500

    game alerts when game is over

--- src/components/quizQuestions.js
+++ src/components/quizQuestions.js
@@ -15,17 +15,37 @@ class QuizQuestion extends Component {
         const answer = e.target.nextSibling.innerHTML;
         console.log(answer);
 
-        if (answer === this.props.allCorrectAnswers[id]) {
-            alert("correct!!");
-            
-        } else {
-            alert("wrong");
-        }
-        
-        this.setState( (prevState) =>
-         { return {currentQuestion: prevState.currentQuestion + 1}})
+       
 
-    } 
+            if (answer === this.props.allCorrectAnswers[id]  ) {
+                alert("correct!!");
+                this.checkEnd();
+            } else {
+                alert("wrong");
+                this.checkEnd();
+                
+            }
+
+           
+
+        }
+
+        
+    checkEnd = () => {
+        if (this.state.currentQuestion < this.props.allCorrectAnswers.length - 1) {
+
+            this.setState( (prevState) =>
+                { return {currentQuestion: prevState.currentQuestion + 1}})
+        } else {
+            alert("game over!!")
+        }
+    }
+        
+       
+
+
+
+    
 
     render() {
             let question = this.props.questions;
