0a765db48e43a00fbe85b461da08345a3a91184b
Alejandro-Sierra <alexsierra1598@gmail.com>
2018-12-26 18:39:07 -0500

    conditional rendered button on makeQuiz.js

--- src/components/makeQuiz.js
+++ src/components/makeQuiz.js
@@ -9,8 +9,21 @@ class MakeQuiz extends Component {
 
 
     render() {
+        let nextBtn;
+
+        if (this.props.title.length > 1) {
+            nextBtn = <Link to="/questions" >
+            <input type="submit" className="btn btn-info" value="next" />
+             </Link>
+        } else {
+            nextBtn = <div></div>
+        }
+
         return (
+
             <div>
+
+                <form required>
 
                 <div className="input-group mb-3">
                 <div className="input-group-prepend">
@@ -19,16 +32,13 @@ class MakeQuiz extends Component {
                 <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"
                 value={this.props.title}
                 onChange={this.props.handleNameChange}
+                required
                 ></input>
                 </div>
 
-               
+               {nextBtn}
+                </form>
 
-                <div>{this.props.numValue}</div>
-
-                <Link to="/questions" >
-                    <button type="button" className="btn btn-info">Continue</button>
-                </Link>
 
             </div>
         )
